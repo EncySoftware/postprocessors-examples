20fb97446fcff5e4176402cad448247fb918060b
cepera-ency <cepera@encycam.io>
2024-12-16 09:17:38 +0300

    XMLPost example

---
+++ Features/OutputFiles/XMLPost/Postprocessor.cs
@@ -0,0 +1,30 @@
+namespace DotnetPostprocessing.Post;
+
+using System.Xml;
+
+public partial class Postprocessor: TPostprocessor
+{
+    string fileName = Path.GetTempFileName(); //@"D:\Work\Files\NewDNPosts\OutFilesDemoPost\TestXMLOutput.xml";
+    XmlDocument doc;
+
+    public override void OnStartProject(ICLDProject prj)
+    {
+        doc = new XmlDocument();
+        var nd = doc.CreateElement("CLDataCommands");
+        doc.AppendChild(nd);
+    }
+    
+    public override void OnBeforeCommandHandle(ICLDCommand cmd, CLDArray cld) 
+    {
+        var nd = doc.CreateElement(cmd.Name);
+        doc.DocumentElement.AppendChild(nd);
+        nd.InnerText = cmd.Caption;
+    }
+
+    public override void OnFinishProject(ICLDProject prj)
+    {
+        doc.Save(fileName);
+        NCFiles.AddExternalFile(fileName, this);
+    }
+
+}
