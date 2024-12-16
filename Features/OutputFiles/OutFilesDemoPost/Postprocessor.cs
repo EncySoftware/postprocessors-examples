c4c956e03f49478b97a9f1aa268af28f19bf9a49
cepera-ency <cepera@encycam.io>
2024-12-16 09:15:29 +0300

    OutFilesDemoPost empty example

---
+++ Features/OutputFiles/OutFilesDemoPost/Postprocessor.cs
@@ -0,0 +1,18 @@
+namespace DotnetPostprocessing.Post;
+
+public partial class Postprocessor: TPostprocessor
+{
+
+    public override void OnStartProject(ICLDProject prj)
+    {
+    }
+    
+    public override void OnBeforeCommandHandle(ICLDCommand cmd, CLDArray cld) 
+    {
+    }
+
+    public override void OnFinishProject(ICLDProject prj)
+    {
+    }
+
+}
