ac0451f5fbf83dcbb6bac3cc26b6e7c719d3b7b7
cepera-ency <cepera@encycam.io>
2024-12-16 09:14:10 +0300

    JSONPost example added

---
+++ Features/OutputFiles/JSONPost/Postprocessor.cs
@@ -0,0 +1,33 @@
+namespace DotnetPostprocessing.Post;
+
+using System.Text.Json;
+
+public partial class Postprocessor: TPostprocessor
+{
+    string fileName = Path.GetTempFileName(); //@"D:\Work\Files\NewDNPosts\JSONPost\TestJSON.json";
+    Utf8JsonWriter file;
+
+    public override void OnStartProject(ICLDProject prj)
+    {
+        FileStream stream = new FileStream(fileName, FileMode.Create);
+        JsonWriterOptions options = new JsonWriterOptions();
+        options.Indented = true;
+        file = new Utf8JsonWriter(stream, options);
+        file.WriteStartObject();
+        file.WriteStartArray("CLDataCommands");
+    }
+    
+    public override void OnBeforeCommandHandle(ICLDCommand cmd, CLDArray cld) 
+    {
+        file.WriteStringValue(cmd.Caption);
+    }
+
+    public override void OnFinishProject(ICLDProject prj)
+    {
+        file.WriteEndArray();
+        file.WriteEndObject();
+        file.Flush();
+        NCFiles.AddExternalFile(fileName, this);
+    }
+
+}
